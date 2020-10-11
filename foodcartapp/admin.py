4b433238e560196901bf2c067de5d511220f1d7c
Denis Orionov <orionovdv@gmail.com>
2020-10-11 21:00:45 +0300

    Add fields OrderItemInline

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -111,7 +111,7 @@ class ProductAdmin(admin.ModelAdmin):
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     extra = 0
-
+    fields = ['product', 'quantity']
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
